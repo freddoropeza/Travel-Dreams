5bcb61f3e715c353983b8c52c501489475e0743d
fredd <fredd.oropeza@gmail.com>
2021-10-11 00:18:56 -0400

    Adds user auth and dashboard

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,25 +1,36 @@
 const Post = require('../models/post');
 const router = require('express').Router();
 const isAuthenticated = require('../utilities/auth');
+// induces
 
-router.get('/post', (req, res) => {
+// Index
+router.get('/dashboard', (req, res) => {
     Post.find({}, (err, posts) => {
-        res.render('posts/index.ejs', { posts, user: req.session.user });
+        res.render('posts/dashboard.ejs');
     });
 });
+// { posts, user: req.session.user }
 
+// New
 router.get('/posts/new', isAuthenticated, (req, res) => {
     res.render('posts/new.ejs');
 });
 
+// Delete
 
+// Update
 
+// Create
 router.post('/posts', isAuthenticated, (req, res) => {
     req.body.author = req.session.user;
     Post.create(req.body, (err, post) => {
-        res.redirect('/posts');
+        res.redirect('/dashboard');
     });
 });
+
+// Edit
+
+// Show
 
 
 module.exports = router;
--- models/post.js
+++ models/post.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const postSchema = new Schema({
     img: String,
     body: String,
-    author: { type: Schema.Types.ObjectId, ref: 'User'}
+    author: { type: Schema.Types.ObjectId, ref: 'User'},
 }, { timestamps: true });
 
 module.exports = mongoose.model('Post', postSchema);
